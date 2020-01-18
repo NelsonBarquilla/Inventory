30e527c3a8c39f6d074220f029946835956ba9f0
NelsonBarquilla <43336993+NelsonBarquilla@users.noreply.github.com>
2020-01-18 17:29:49 +0800

    Add files via upload
    Successfully connected to database using MySQL

--- node.js
+++ node.js
@@ -4,3 +4,21 @@ const bodyparser = require('body-parser');
 
 var app = express();
 app.use(bodyparser.json());
+
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'inventory',
+    multipleStatements: true
+});
+
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('DB connection succeded');
+    else
+        console.log('DB connection failed');
+});
+
+
+app.listen(3000);
